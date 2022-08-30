466a741c6def9f8b3e6774d1e4328a22d1cea7d3
Md. Rony <rony15-10527@diu.edu.bd>
2022-08-30 10:27:39 +0600

    Updated file name

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,8 +1,8 @@
 const Product = require("../models/products.model");
 
 //GET all product at endpoint'/product'
-const allProduct = async (req, res) => {
-     await Product.find({})
+module.exports.allProduct = (req, res) => {
+     Product.find({})
        .exec((err, data) => {
          if (err) {
            res.status(500).json({
@@ -18,20 +18,20 @@ const allProduct = async (req, res) => {
 };
      
 //POST  all the product at endpoint '/product'
-const newProduct = async (req, res) => {
-      await Product.insertMany(req.body, (err) => {
-        if (err) {
-          res.status(500).json({
-            error: "There was a server side error!",
-          });
-        } else {
-          res.status(200).json({
-            message: "Products were inserted successfully!",
-          });
-        }
-      }); 
+module.exports.newProduct = (req, res) => {
+  Product.insertMany(req.body, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Products were inserted successfully!",
+      });
+    }
+  });
 };
 
 
 
-module.exports = { newProduct,allProduct };
+
